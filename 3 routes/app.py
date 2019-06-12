eda928d1b46b42eda39735dc58ba56a150c1a128
Marlon Inga C <marlon.inga@metricaandina.com>
2019-06-12 17:18:14 -0500

    feat(routes): creating routes with args of types

--- 3 routes/app.py
+++ 3 routes/app.py
@@ -8,9 +8,35 @@ def index():
     return 'Hello World!'
 
 
-@app.route('/hola')
-def hola():
-    return 'Hola'
+@app.route('/hello')
+def hello():
+    return 'Hello'
+
+
+@app.route('/user/<string:username>')
+def user(username):
+    return 'Hello ' + username
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return 'Number: {}'.format(n)
+
+
+@app.route('/user/<int:id>/<string:username>')
+def user_name(id, username):
+    return 'Id: {} and User name: {}'.format(id, username)
+
+
+@app.route('/sum/<float:n1>/<float:n2>')
+def sum(n1, n2):
+    return "N1: {}, N2: {}, Sum: {}".format(n1, n2, n1 + n2)
+
+
+@app.route('/default/')
+@app.route('/default/<string:dft>')
+def default(dft='default'):
+    return 'The value of dft is: ' + dft
 
 
 if __name__ == "__main__":
